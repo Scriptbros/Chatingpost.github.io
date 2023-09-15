5d2bae0c35f7ebbb040591369677be3fbce0c98a
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 16:51:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ function agree(element) {
 function disagree(element) {
     element.previousSibling.innerHTML = 'Disagreed!';
 }
+
+
+document.getElementById('postForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    var postContent = document.getElementById('postContent').value;
+    var postList = document.getElementById('postList');
+
+    var postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.innerHTML = `
+        <p>${postContent}</p>
+        <button class="agree" onclick="agree(this)">Agree</button>
+        <button class="disagree" onclick="disagree(this)">Disagree</button>
+        <div class="counter">Agreed: <span>0</span></div>
+    `;
+
+    postList.appendChild(postElement);
+});
+
+function agree(element) {
+    var counter = element.nextSibling.nextSibling.querySelector('span');
+    var count = parseInt(counter.textContent, 10);
+    counter.textContent = count + 1;
+}
+
+function disagree(element) {
+    var counter = element.nextSibling.querySelector('span');
+    var count = parseInt(counter.textContent, 10);
+    counter.textContent = count + 1;
+                                                     }
