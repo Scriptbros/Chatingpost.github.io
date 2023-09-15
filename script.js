30da68e826e77ead042145561382b1464f18973c
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 16:54:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,32 +8,6 @@ document.getElementById('postForm').addEventListener('submit', function(event) {
     postElement.classList.add('post');
     postElement.innerHTML = `
         <p>${postContent}</p>
-        <button onclick="agree(this)">Agree</button>
-        <button onclick="disagree(this)">Disagree</button>
-    `;
-
-    postList.appendChild(postElement);
-});
-
-function agree(element) {
-    element.previousSibling.innerHTML = 'Agreed!';
-}
-
-function disagree(element) {
-    element.previousSibling.innerHTML = 'Disagreed!';
-}
-
-
-document.getElementById('postForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    var postContent = document.getElementById('postContent').value;
-    var postList = document.getElementById('postList');
-
-    var postElement = document.createElement('div');
-    postElement.classList.add('post');
-    postElement.innerHTML = `
-        <p>${postContent}</p>
         <button class="agree" onclick="agree(this)">Agree</button>
         <button class="disagree" onclick="disagree(this)">Disagree</button>
         <div class="counter">Agreed: <span>0</span></div>
@@ -52,4 +26,4 @@ function disagree(element) {
     var counter = element.nextSibling.querySelector('span');
     var count = parseInt(counter.textContent, 10);
     counter.textContent = count + 1;
-                                                     }
+}
