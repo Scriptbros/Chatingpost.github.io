22b72bcf1d1a42c6b3abc49bbf07d2b5f10fbc11
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 16:38:49 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/your_database', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+// Event handlers for connection events
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+
+module.exports = db;
